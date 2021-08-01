52647c8718ac9cc2455107b25f8b4a809d54e49e
ocean_wll <wanglinglong@shulie.io>
2021-08-01 16:04:21 +0800

    init

---
+++ src/main/java/pers/ocean/samplemq/SampleMqApplication.java
@@ -0,0 +1,13 @@
+package pers.ocean.samplemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/ocean/samplemq/SampleMqApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.ocean.samplemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
